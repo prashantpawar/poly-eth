2404d3c0e35183c61c0490258bfb0e7086c6743b
d11e9 <d11e9@mailinator.com>
2014-08-13 16:02:07 +0100

    Import ethstring and bigint

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-(function (root, factory) {
-  if (typeof exports === 'object') {
-    // CommonJS
-    module.exports = factory(require('b'));
-  } else if (typeof define === 'function' && define.amd) {
-    // AMD
-    define(['b'], function (b) {
-      return (root.returnExportsGlobal = factory(b));
-    });
-  } else {
-    // Global Variables
-    root.returnExportsGlobal = factory(root.b);
-  }
-}(this, function (b) {
-  // Your actual module
+
+var bigInt = require( './lib/BigInteger' );
+var EthString = require( './lib/ethstring' );
+
+
+
+
+var polyeth = function(eth) {
 
   var checkClient = function(eth) {
     var UA = window.navigator.userAgent;
@@ -20,30 +13,30 @@
     if (UA.match( 'Ethereal')) return 'ethereal'
   }
 
-  return polyeth = function(eth){
+  var mocketh = {
+    eth: null,
+    getKey: function(cb){ cb('MockKey213dsf3454as')}
+  }
 
-    var mocketh = {
-      eth: null,
-      getKey: function(cb){ cb('MockKey213dsf3454as')}
+  var clients = {
+    aleth: {
+      eth: eth,
+      client: 'aleth',
+      getKey: function(cb){ return cb(eth.key); }
+    },
+
+    ethereal: {
+      eth: eth,
+      client: 'ethereal',
+      getKey: function(cb){ return eth.getKey(cb); }
     }
+  }
 
-    var clients = {
-      aleth: {
-        eth: eth,
-        client: 'aleth',
-        getKey: function(cb){ return cb(eth.key); }
-      },
+  if (!eth) return mocketh;
+  else return clients[checkClient(eth)]
+};
 
-      ethereal: {
-        eth: eth,
-        client: 'ethereal',
-        getKey: function(cb){ return eth.getKey(cb); }
-      }
-    }
 
-    if (!eth) return mocketh;
-    
-    console.log( 'Polyeth loaded. wrapping/polyfilling native eth object.');
-    return clients[checkClient(eth)]
-  };
-}));
+if(typeof module !== 'undefined' ) {
+  module.exports = polyeth;
+}
