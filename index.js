472fc144244fec7c0ddbbc1635943279737a9290
Your Name <you@example.com>
2014-08-27 14:12:24 +0100

    proxy mocketh apis to cpp-rpc and add ethos UA

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ var bigInt = require( './lib/BigInteger' );
 var EthString = require( './lib/ethstring' );
 
 
-
-
 var polyeth = function(eth) {
 
   var checkClient = function(eth) {
     var UA = window.navigator.userAgent;
     if (UA.match( 'Aleth' )) return 'aleth';
-    if (UA.match( 'Ethereal')) return 'ethereal'
+    if (UA.match( 'Ethereal')) return 'ethereal';
+    if (UA.match( 'Ethos')) return 'ethos';
   }
 
   var accounts = {
@@ -21,9 +20,41 @@ var polyeth = function(eth) {
 
   var watching = {}
 
+  var cpp_eth = require( './lib/eth.js')
+
   var mocketh = {
-    eth: require( './lib/eth.js'),
+    eth: cpp_eth,
     client: 'mocketh',
+
+    // Proxy to cpp-ethereum eth/rpc object
+
+    getProcedures: cpp_eth.getProcedures,
+    getCoinbase: cpp_eth.getCoinbase,
+    getIsListening: cpp_eth.getIsListening,
+    getIsMining: cpp_eth.getIsMining,
+    getGasPrice: cpp_eth.getGasPrice,
+    getKey: cpp_eth.getKey,
+    getKeys: cpp_eth.getKeys,
+    getPeerCount: cpp_eth.getPeerCount,
+    getBalanceAt: cpp_eth.getBalanceAt,
+    balanceAt: cpp_eth.balanceAt,
+    getStorageAt: cpp_eth.getStorageAt,
+    storageAt: cpp_eth.storageAt,
+    getTxCountAt: cpp_eth.getTxCountAt,
+    txCountAt: cpp_eth.txCountAt, 
+    getIsContractAt: cpp_eth.getIsContractAt,
+    isContractAt: cpp_eth.isContractAt,
+    create: cpp_eth.create,
+    transact: cpp_eth.transact,
+    getSecretToAddress: cpp_eth.getSecretToAddress,
+    secretToAddress: cpp_eth.secretToAddress,
+    getLll: cpp_eth.getLll,
+    lll: cpp_eth.lll,
+    check: cpp_eth.check,
+    watch: cpp_eth.watch,
+    unwatch: cpp_eth.unwatch,
+    newBlock: cpp_eth.newBlock,
+
     getKeys: function(cb){ cb(['MockKey213dsf3454as'])},
     secretToAddress: function(privateKey){
       return accounts[privateKey] || accounts['SELF'];
@@ -42,6 +73,9 @@ var polyeth = function(eth) {
   }
 
   var clients = {
+
+    ethos: checkClient( eth ) == 'aleth' && mocketh,
+
     aleth: checkClient( eth ) == 'aleth' && {
       eth: eth,
       client: 'aleth',
